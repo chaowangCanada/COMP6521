df8bfc766a10c82626d180dd0f44bedc2e8bd550
chaowangcanada <chaowang.ubc@gmail.com>
2017-02-06 23:33:55 -0500

    source code

---
+++ src/Metadata.java
@@ -0,0 +1,61 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Metadata {
+	
+	long rec_size;
+	long N;
+	long M;
+	long ram;
+	int cols;
+	int[] sort_cols;
+	String mode;
+	
+	public Metadata(String filename,long mem,int[] sortcolumns,String smode){
+		try {
+			mode=smode;
+			ram = mem;
+			sort_cols=sortcolumns;
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+			cols=0;
+			
+			String line;
+			StringTokenizer tc;
+			rec_size=0;
+			while((line=br.readLine()) != null){
+				cols++;
+				
+				tc = new StringTokenizer(line,",");
+				while(tc.hasMoreTokens()){
+					String s = tc.nextToken();
+					s = tc.nextToken();
+					rec_size = rec_size +  Integer.parseInt(s);
+				}
+				
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	long getRecordSize(){
+		return 5*rec_size;
+	}
+	
+	long getAllowedRAM(){
+		return ram;
+	}
+
+	public int[] getSortFields() {
+		// TODO Auto-generated method stub
+		return sort_cols;
+	}
+
+	public String getMode() {
+		return mode;
+	}
+}
---
+++ src/TwoPhaseMerger.java
@@ -0,0 +1,22 @@
+
+
+import java.util.ArrayList;
+
+public class TwoPhaseMerger { 	
+
+	
+		public static void main(String[] st){
+			int[] sort_cols = new int[st.length-4];
+			for(int i=4;i<st.length;i++){
+				sort_cols[i-4]=Integer.parseInt(st[i].substring(st[i].length()-1));
+			}
+			System.out.println("1 "+ Runtime.getRuntime().freeMemory());
+			Metadata metadata = new Metadata("samples/sample1/metadata.txt",(long)(Runtime.getRuntime().freeMemory()*0.4),sort_cols,st[3]);
+			ArrayList<String> tempfiles;
+			Partition pr  = new Partition(metadata,"samples/sample1/input.txt");
+			System.out.println("3 "+ Runtime.getRuntime().freeMemory());
+			tempfiles = pr.divideAndSort();
+			
+			
+		}
+}
